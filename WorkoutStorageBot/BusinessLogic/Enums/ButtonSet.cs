c7b4acce34f10e755b27d91b96f077b2a3bb4401
Grawter <terminator17067@mail.ru>
2024-06-02 21:33:32 +0300

    Частичное добавление экспорта тренировок в json; фикс null ref при считывания конфигурации на старте

--- WorkoutStorageBot/BusinessLogic/Enums/ButtonSet.cs
+++ WorkoutStorageBot/BusinessLogic/Enums/ButtonSet.cs
@@ -35,6 +35,7 @@ namespace WorkoutStorageBot.BusinessLogic.Enums
                                                 ArchiveExercisesList,
                                                 ConfirmDeleteExercise,
             ArchiveList,
+            Export,
             ConfirmDeleteAccount,
 
         Period,
---
+++ WorkoutStorageBot/Helpers/Export/CommonExportHelper.cs
@@ -0,0 +1,32 @@
+﻿using WorkoutStorageBot.Model;
+
+namespace WorkoutStorageBot.Helpers.Export
+{
+    internal static class CommonExportHelper
+    {
+        internal static DateTime GetFilterDateTime(int monthFilterPeriod, IQueryable<ResultExercise> fromDBData)
+        {
+            DateTime filterDateTime = DateTime.MinValue;
+
+            if (monthFilterPeriod > 0)
+            {
+                filterDateTime = fromDBData
+                                        .Select(r => r.DateTime)
+                                        .Max()
+                                        .AddMonths(-monthFilterPeriod);            
+            }
+
+            return filterDateTime;
+
+        }
+
+        internal static void LoadDBDataToDBContextForFilterDate (IQueryable<ResultExercise> sourceDBData, DateTime filterDate)
+        {
+            sourceDBData.Where(re => 
+                                filterDate > DateTime.MinValue
+                                    ? re.DateTime >= filterDate
+                                    : true)
+                        .ToList();
+        }
+    }
+}
